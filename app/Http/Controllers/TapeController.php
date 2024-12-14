1c58c5105c7872fb8f976da62f75ba15c7b8a82d
Alexander <you@example.com>
2024-12-13 22:25:58 -0600

    Just a change

--- app/Http/Controllers/TapeController.php
+++ app/Http/Controllers/TapeController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tape;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
 class TapeController extends Controller
 {
     function getTape($id) {
-        return Storage::download(Tape::where('id', $id)->first()->file);
+        $tape = Tape::where('id', $id)->first();
+        return response(Storage::get($tape->file))->header('Content-Disposition', 'attachment; filename="' . $tape->filename . '"');
     }
 }
